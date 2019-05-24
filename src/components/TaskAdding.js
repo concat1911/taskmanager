f9a0aa40e6c1d158ccf0ca0dcff901a214535781
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-24 23:27:19 +0200

    Update TaskAdding.js

--- src/components/TaskAdding.js
+++ src/components/TaskAdding.js
@@ -2,17 +2,39 @@ import React, {useState, useContext} from 'react';
 import TasksContext from '../context/index';
 
 export default function TaskAdding (){
+
+    const [taskName, SetTaskName] = useState("");
+    const [taskDescription, SetTaskDescription] = useState("");
+    const [taskLevel, SetTaskLevel] = useState(1);
+
+    const {dispatch} = useContext(TasksContext);
+
+    const HandleSubmit = (event) => {
+        event.preventDefault();
+
+        const newTask = {
+            name: taskName,
+            description: taskDescription,
+            level: taskLevel
+        }
+
+        dispatch({type: "TASK_ADDING", paload: newTask});
+        SetTaskName("");
+        SetTaskDescription("");
+        SetTaskLevel("");
+    }
+
     return (
-        <form className="ui form" >
+        <form className="ui form" onSubmit={HandleSubmit}>
             <div className="fields">
                 <div className="four wide field">
-                    <input name="taskName" type="text" placeholder="Task Name" />
+                    <input name="taskName" type="text" placeholder="Task Name" onChange={event => SetTaskName(event.target.value)} value={taskName}/>
                 </div>
                 <div className="six wide field">
-                    <input name="taskDescription" type="text" placeholder="Description"/>
+                    <input name="taskDescription" type="text" placeholder="Description" onChange={event => SetTaskDescription(event.target.value)} value={taskDescription}/>
                 </div>
                 <div className="field">
-                    <select name="level" className="ui dropdown">
+                    <select name="level" className="ui dropdown" onChange={event => SetTaskLevel(event.target.value)}>
                         <option value="0">low</option>
                         <option value="1">medium</option>
                         <option value="2">high</option>
