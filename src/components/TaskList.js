500ecc0dec27a313150d46b8f446efede62e5429
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-25 15:26:52 +0200

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,8 +5,20 @@ import { Modal } from 'semantic-ui-react';
 export default function TaskList(){
 
     const {state, dispatch} = useContext(TasksContext);
-    //MODAL
+    
+    //EDITOR MODAL
+    const [nameEditor, SetNameEditor] = useState("");
+    const [descripEditor, SetDescripEditor] = useState("");
+    const [levelEditor, SetLevelEditor] = useState(0);
+
     const [modalOpen, ToggleModal] = useState(false);
+
+    const OnModalOpen = task => {
+        SetNameEditor(task.name);
+        SetDescripEditor(task.description);
+        SetLevelEditor(task.level);
+        ToggleModal(true);
+    }
 
     return(
         <div className="ui">
@@ -21,39 +33,44 @@ export default function TaskList(){
                             <h2 className="ui header">{task.description}</h2>
                         </div>
                         <div className="extra content">
-                            <button className="ui button" onClick={() => ToggleModal(true)}>Edit</button>
+                            <button className="ui button" onClick={() => OnModalOpen(task)}>Edit</button>
                             <button className="ui button" onClick={() => dispatch({type: "REMOVE_TASK", payload: task})}>Remove</button>
                         </div>
+
+                        {/* MODAL */}
+                        <Modal open={modalOpen} onClose={() => ToggleModal(false)}>
+                            <Modal.Header>Edit Task</Modal.Header>
+                            <Modal.Content image>
+                                <Modal.Description>
+                                    <form className="ui form">
+                                        <div className="fields">
+                                            <div className="four wide field">
+                                                <input name="editName" type="text" placeholder="Task Name" value={nameEditor}/>
+                                            </div>
+                                            <div className="six wide field">
+                                                <input 
+                                                    name="editDescription" 
+                                                    type="text" 
+                                                    placeholder="Description" 
+                                                    value={descripEditor} 
+                                            />
+                                            </div>
+                                            <div className="field">
+                                                <select name="editLevel" className="ui dropdown" value={levelEditor}>
+                                                    <option value="0">low</option>
+                                                    <option value="1">medium</option>
+                                                    <option value="2">high</option>
+                                                </select>
+                                            </div>
+                                            <button className="ui button" type="submit"><i className="add icon"></i>Change</button>
+                                        </div>
+                                    </form>
+                                </Modal.Description>
+                            </Modal.Content>
+                        </Modal> 
                     </div>
                 ))}
-            </div>
-
-            {/* MODAL */}
-            <Modal open={modalOpen} onClose={() => ToggleModal(false)}>
-                <Modal.Header>Edit Task</Modal.Header>
-                <Modal.Content image>
-                    <Modal.Description>
-                        <form className="ui form">
-                            <div className="fields">
-                                <div className="four wide field">
-                                    <input name="editName" type="text" placeholder="Task Name"/>
-                                </div>
-                                <div className="six wide field">
-                                    <input name="editDescription" type="text" placeholder="Description" />
-                                </div>
-                                <div className="field">
-                                    <select name="editLevel" className="ui dropdown">
-                                        <option value="0">low</option>
-                                        <option value="1">medium</option>
-                                        <option value="2">high</option>
-                                    </select>
-                                </div>
-                                <button className="ui button" type="submit"><i className="add icon"></i>Change</button>
-                            </div>
-                        </form>
-                    </Modal.Description>
-                </Modal.Content>
-            </Modal>  
+            </div> 
         </div>
     );
 }
