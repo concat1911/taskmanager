3421e2e429f735cbff44f5398109705b200a761c
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-28 18:45:28 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+//Index Route
+app.get('/', (req, res) => {
+    res.send('INDEX');
+})
+
+//About
+app.get('/about', (req, res) => {
+    res.send('About');
+})
+
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
 });
