94601bffec51e53525ddce81073ac0939c3d2f0e
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-29 20:47:19 +0200

    Mongo db update

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 
+//MONGODB CONNECT
+const mongoose = require('mongoose');
+
+//PASSPORT
+require('./services/passport');
+
+//AUTH ROUTES
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
-
+authRoutes(app);
+//another way: require('./routes/authRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+    //Index Route
+    app.get('/', (req, res) => {
+        res.send({homepage: ''});
+    })
+    
+    //GOOGLE API
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+    
+    app.get('/auth/google/callback', passport.authenticate('google'))
+}
