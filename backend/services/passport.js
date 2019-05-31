03911f639857c809de01daa87ca6a3431c7cdaa1
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-31 21:39:01 +0200

    Update passport.js

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -23,18 +23,15 @@ passport.use(new GoogleStrategy(
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser =  await User.findOne({googleId: profile.id})
 
-        User.findOne({googleId: profile.id})
-            .then((existingUser) => {
-                if(existingUser){
-                    //User already in DB
-                    done(null, existingUser);
-                }else{
-                    //This is new User
-                    new User({googleId: profile.id}).save()
-                        .then(user => done(null, user));
-                }
-            })
+        if(existingUser){
+            return done(null, existingUser);
+        } 
+        //This is new User
+        const user = await new User({googleId: profile.id}).save()
+        done(null, user);           
     })
 );
