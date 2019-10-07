a7469d2bfb2e565b0e6351dc2d521a7fa8e4b440
Tran Nhat Linh <49843263+concat1911@users.noreply.github.com>
2019-10-07 16:13:58 +0200

    Delete secrets.js

--- backend/src/secrets.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongoURL : 'mongodb+srv://linh:onlyme2511@cluster-eb3gs.mongodb.net/test?retryWrites=true&w=majority',
-    gmailAcc: {
-        name: 'blackromancevn@gmail.com',
-        password: 'onlyme2511'
-    },
-    jwt_scret: 'NOWOMANNOCRIME'
-}
